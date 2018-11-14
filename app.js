f3a88d10cc895990c18a548cff059001496ae7fe
Villalba <gianluca.nadir.villalba@capgemini.com>
2018-11-14 17:54:40 +0100

    Parsing Request Bodies

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ const server = http.createServer((req, res) => {
         return res.end()
     }
     if (url === '/message' && method === 'POST') {
+        const body = []
+        req.on('data', (chunk) => {
+          console.log(chunk)
+          body.push(chunk)  
+        })
+        req.on('end', () => {
+            const parseBody = Buffer.concat(body).toString()
+            const message = parseBody.split('=')[1]
+            fs.writeFileSync('message.txt', message)
+        })
         fs.writeFileSync('message.txt', 'DUMMY')
         res.statusCode = 302
         res.setHeader('Location', '/')
