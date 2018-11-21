b941cecd5fa99219990dd3b008a949dcbad4ff21
RidanDev <gianlucanadir@gmail.com>
2018-11-21 07:53:33 +0100

    comments added

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const http = require('http')
 const fs = require('fs')
 
+/*
+createServer() should be executed for every incoming request
+*/
 const server = http.createServer((req, res) => {
     const url = req.url
     const method = req.method
+    /*
+    The incoming data is sent as a stream of data.
+    Our stream is basically an ongoiong process
+    the request is simply read by node in chunks (multiple parts).
+    This is done so that we theoretically can start working on this
+    on individual chunks without having to wait for the full request 
+    being read.
+    Considering a file being uploaded, this will take considerably longer and therefore
+    streaming that data could make sense because it could allow you to start
+    writing this to your disk, so you don't have to wait for it being fully 
+    uploaded before you can do anything with it.
+
+    To organize these incoming chunks, you use a so-call buffer. 
+    A buffer is like a bus stop which allows you to hold multiple chunks
+    and work with them before they are released once you're done
+    */
     if (url === '/') {
         res.write('<html>')
         res.write('<head><title>Enter Message</title></head>')
@@ -13,16 +32,26 @@ const server = http.createServer((req, res) => {
     }
     if (url === '/message' && method === 'POST') {
         const body = []
+        /*
+        req.on() allows us to listen to certain events,
+        and the event I want to listen to here is the data event.
+        Now we are defining a function to be executed for every incoming data piece.
+        */
         req.on('data', (chunk) => {
           console.log(chunk)
           body.push(chunk)  
         })
+        /*
+        The end listener will be fired once it's done parsing the incoming requests data
+        or the incoming requests in general
+        */
         req.on('end', () => {
-            const parseBody = Buffer.concat(body).toString()
-            const message = parseBody.split('=')[1]
+            //I'm able to convert it to a string because I know the user's input is a a text
+            const parsedBody = Buffer.concat(body).toString()
+            console.log(parsedBody)
+            const message = parsedBody.split('=')[1]
             fs.writeFileSync('message.txt', message)
         })
-        fs.writeFileSync('message.txt', 'DUMMY')
         res.statusCode = 302
         res.setHeader('Location', '/')
         return res.end()
