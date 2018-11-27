e3e203bdd9346a74f93581f432dc803a5646a4ed
Villalba <gianluca.nadir.villalba@capgemini.com>
2018-11-27 09:38:50 +0100

    Adding Middleware

--- app.js
+++ app.js
@@ -4,5 +4,14 @@ const express = require('express')
 
 const app = express()
 
+app.use((req, res, next) => {
+    console.log('In the middleware')
+    next() //Allows the request to continue to next middleware in line
+})
+
+app.use((req, res, next) => {
+    console.log('In another middleware')
+})
+
 const server = http.createServer(app)
 server.listen(3000)
