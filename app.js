40f96e26e4501181411822ddcde2ca71442f81ee
RidanDev <gianlucanadir@gmail.com>
2018-12-17 22:57:46 +0100

    Just commented a bit

--- app.js
+++ app.js
@@ -8,11 +8,16 @@ const shopRoutes = require('./routes/shop')
 const app = express()
 
 app.set('view engine', 'ejs')
+
+//Allows me to refer to a file inside the views folder just typing its name
+//i.e 404 => views/404.ejs
 app.set('views', 'views')
 
+//Parses the body making easier to access an element
+//i.e req.body.title
 app.use(bodyParser.urlencoded({ extended: false }))
 
-//forwards the request to the public folder allowing me to access the elements inside
+//Forwards the request to the public folder allowing me to access the elements inside
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin', adminData.routes)
