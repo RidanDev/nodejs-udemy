62b7f345974f38f2ca0383ea1569c3028e9bb6a2
Villalba <gianluca.nadir.villalba@capgemini.com>
2018-12-19 14:22:17 +0100

    Adding Controllers

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 
-const adminData = require('./routes/admin')
+const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 const app = express()
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 //forwards the request to the public folder allowing me to access the elements inside
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminData.routes)
+app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
 app.use('/', (req, res, next) => {
