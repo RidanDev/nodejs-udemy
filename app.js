38d11657551c8371aa4efd360afdc96487df5585
RidanDev <gianlucanadir@gmail.com>
2018-12-20 22:55:34 +0100

    Storing Data in Files Via the Model

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 
-const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+const adminRoutes = require('./routes/admin')
 
 const errorController = require('./controllers/error')
 
@@ -17,8 +17,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 //forwards the request to the public folder allowing me to access the elements inside
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoutes)
 app.use(shopRoutes)
+app.use('/admin', adminRoutes)
 app.use(errorController.get404)
 
 app.listen(3000)
