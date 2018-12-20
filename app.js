e34f1f50f49296ab11632b716dc89e87bd6f6401
Villalba <gianluca.nadir.villalba@capgemini.com>
2018-12-20 11:45:24 +0100

    Finishing the Controllers

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser')
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
+const errorController = require('./controllers/error')
+
 const app = express()
 
 app.set('view engine', 'ejs')
@@ -17,9 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
-
-app.use('/', (req, res, next) => {
-    res.status(404).render("404", { pageTitle: "Page Not Found", path: '' })
-})
+app.use(errorController.get404)
 
 app.listen(3000)
