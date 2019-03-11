d5fad28daab06db31bbb41dd570b9f022ea4a36b
RidanDev <gianlucanadir@gmail.com>
2019-03-11 23:48:03 +0100

    set up the project

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,9 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("add-product", {
+  res.render("admin/add-product", {
     pageTitle: "Add Product",
-    path: "/admin/add-product",
+    path: "admin/add-product",
     formsCSS: true,
     productCSS: true,
     activeAddProduct: true
@@ -71,7 +71,7 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render("shop", {
+    res.render("shop/product-list", {
       prods: products,
       pageTitle: "Shop",
       path: "/",
